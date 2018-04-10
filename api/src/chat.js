dbbf7c3e24282688363b759090078f2a79e6154e
Shubham Gupta <shubhamg2@hexaware.com>
2018-04-10 18:43:51 +0530

    getting comments from tickect added

---
+++ api/src/chat.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+module.exports = {
+    "getComments": function(ticketNumber, callback) {
+
+    var options = { 
+        method: 'GET',
+        url: 'https://humanbot.zendesk.com/api/v2/tickets/63/comments.json',
+        headers: { 
+            'Content-Type': 'application/json',
+        },
+        auth:{
+            'user': 'wrestlingmania9@gmail.com',
+            'pass':'Password@123'
+        }
+    };
+        
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            callback(null, response.body);
+        }
+    });
+    }
+}
--- index.js
+++ index.js
@@ -1,70 +1,52 @@
-// var express = require('express');
-// var app = express();
-// var zendesk = require('node-zendesk');
-// var exampleConfig = require('./config');
-// var db = require('./api/db/mysql.js');
-// //var fs = require('fs');
-// //var zd = require('../lib/client');
-// var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var zendesk = require('node-zendesk');
+var exampleConfig = require('./config');
+var db = require('./api/db/mysql.js');
+var chatComments = require('./api/src/chat')
+//var fs = require('fs');
+//var zd = require('../lib/client');
+var port = process.env.PORT || 3000;
 
-// var client = zendesk.createClient({
-//   username:  exampleConfig.auth.username,
-//   token:     exampleConfig.auth.token,
-//   remoteUri: exampleConfig.auth.remoteUri
-// });
+var client = zendesk.createClient({
+  username:  exampleConfig.auth.username,
+  token:     exampleConfig.auth.token,
+  remoteUri: exampleConfig.auth.remoteUri
+});
 
-// function listTickets() {
-//     client.tickets.list(function (err, statusList, body, responseList, resultList) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-
-//         body.forEach(function(element) {
-//             var data = {
-//                 ticket_id: element.id,
-//                 via_channel: element.via.channel,
-//                 priority: element.priority,
-//                 status: element.status,
-//                 creation_time: element.created_at
-//             }
-//             var response = db.insertRecord(data, function(response) {
-//             });
-//         })
-//     })
-//     setTimeout(listTickets, 60000);
-//     console.log("called");
-// }
-
-// listTickets();
-
-// app.listen(port);
-// console.log("Server running at port " + port);
-
-const request = require('request');
-
-var options = { 
-                method: 'GET',
-                url: 'https://humanbot.zendesk.com/api/v2/tickets/63/comments.json',
-                headers: { 
-                    'Content-Type': 'application/json',
-                },
-                auth:{
-                    'user': 'wrestlingmania9@gmail.com',
-                    'pass':'Password@123'
-                }
-            };
-        
-    request(options, function (error, response, body) {
-        if (error) {
-            //callback(null,error)
-            //console.log(error.message)
-            console.log(error);
-        } else {
-            //callback(body,null);
-            console.log(JSON.parse(response.body));
-            console.log("--------------");
+function listTickets() {
+    client.tickets.list(function (err, statusList, body, responseList, resultList) {
+        if (err) {
+            console.log(err);
+            return;
         }
-        //console.log(JSON.stringify(body));
-        //console.log(JSON.parse(body));
-    });
+
+        body.forEach(function(element) {
+            if(element.via.channel == 'facebook') {
+                let commentsResponse = deasync(function(callback){
+                    chatComments.getComments(element.id, callback);
+                })();
+                
+                commentResponse.comments.forEach(function(element2){
+                    console.log(element2.plain_body);
+                });
+                var data = {
+                    ticket_id: element.id,
+                    via_channel: element.via.channel,
+                    priority: element.priority,
+                    status: element.status,
+                    creation_time: element.created_at
+                }
+                var response = db.insertRecord(data, function(response) {
+                });
+            }
+        })
+    })
+    setTimeout(listTickets, 60000);
+    console.log("called");
+}
+
+listTickets();
+
+app.listen(port);
+console.log("Server running at port " + port);
