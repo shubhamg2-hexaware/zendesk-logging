9b5e6fb5112ba9100fcb4f97331c8474efc286a6
shubhamg2-hexaware <33827605+shubhamg2-hexaware@users.noreply.github.com>
2019-12-11 15:16:12 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-exports.auth = {
-  username: 'wrestlingmania9@gmail.com',
-  token: 'CIRIKdRSPw51G9ugyrAzzKDTwJbc7iLdFrnix1ay',
-  remoteUri: 'https://humanbot.zendesk.com/api/v2'
-};
